a648b32cf797aaffda3c12143b4ef2e15739ae6e
Igor <chumikov.igor@mail.ru>
2023-10-08 13:03:27 +0700

    Add LocationsPreviewView

---
+++ SwiftfulMapApp/Views/LocationsPreviewView.swift
@@ -0,0 +1,96 @@
+//
+//  LocationsPreviewView.swift
+//  SwiftfulMapApp
+//
+//  Created by Игорь Чумиков on 08.10.2023.
+//
+
+import SwiftUI
+
+struct LocationsPreviewView: View {
+    
+    let location: Location
+    
+    var body: some View {
+        HStack(alignment: .bottom, spacing: .zero) {
+            VStack(alignment: .leading, spacing: 16) {
+                imageSection
+                titleSection
+            }
+            
+            VStack(spacing: 8) {
+                learnMoreButton
+                nextButton
+            }
+            
+        }
+        .padding(20)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.ultraThinMaterial)
+                .offset(y: 65)
+        )
+        .cornerRadius(10)
+    }
+}
+
+extension LocationsPreviewView {
+    private var imageSection: some View {
+        ZStack {
+            if let imageNames = location.imageNames.first {
+                Image(imageNames)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                
+            }
+        }
+        .padding(6)
+        .background(Color.white)
+        .cornerRadius(10)
+    }
+    
+    private var titleSection: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(location.name)
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Text(location.cityName)
+                .font(.subheadline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var learnMoreButton: some View {
+        Button {
+        } label: {
+            Text("Learn more")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View {
+        Button {
+        } label: {
+            Text("Next")
+                .font(.headline)
+                .frame(width: 125, height: 35)
+        }
+        .buttonStyle(.bordered)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.green.ignoresSafeArea()
+        
+        LocationsPreviewView(
+            location: LocationsDataService.locations.first!
+        )
+        .padding(16)
+    }
+}
